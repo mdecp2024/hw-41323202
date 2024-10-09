4cc649f4ac570ccb5497a417a4f587c322d977ee
41323202 <41323202@nfu.edu.tw>
2024-10-09 04:10:02 +0000

    10.9

---
+++ downloads/triangle_star.py
@@ -0,0 +1,18 @@
+#單行註解
+#help(print)
+'''
+123
+'''
+a="一個字串"
+a=24
+a=24.5
+print(a)
+b=print(a,a,a,sep='*')
+print(b)
+a="*"
+b=" "
+print(b,b,b,b,a)
+print(b,b,b,a,b,a)
+print(b,b,a,b,a,b,a)
+print(b,a,b,a,b,a,b,a,)
+print(a,b,a,b,a,b,a,b,a)
